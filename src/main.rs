b81ddce465c31ac55048954308c5f9c404e4dd66
Kamachi <hugo.berendi@outlook.de>
2022-11-19 11:40:25 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -9,53 +9,51 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+    }
+
+    // send a message when a member joins the server
+    async fn guild_member_addition(&self, ctx: Context, guild: Guild, new_member: Member) {
+        let guild = guild.id.to_guild_cached(&ctx.cache).await.unwrap();
+        let channel_id = guild
+            .channels
+            .values()
+            .find(|c| c.name == "🌊・join")
+            .unwrap()
+            .id;
+
+        if let Err(why) = channel_id.say(&ctx.http, format!("Welcome to the server, {}!", new_member.display_name())).await {
+            println!("Error sending message: {:?}", why);
+        }
+
+        // send a privat message to the new member
+        if let Err(why) = new_member.user.direct_message(&ctx, |m| {
+            m.content("Welcome to the server! \n Chat in <#990521519283273730> and send your memes etc. in <#1043471832365416518>");
+        }).await {
+            println!("Error sending message: {:?}", why);
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
-    // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
